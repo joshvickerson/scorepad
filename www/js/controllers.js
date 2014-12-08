348f18eb99cbe26f0fb5ee77b47508647df01e58
Josh Vickerson <josh@joshvickerson.com>
2014-12-08 17:17:12 -0500

    Basic tap detection setup, not doing much of anything yet

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -8,6 +8,8 @@ angular.module('scorepad.controllers', [])
     $scope.scorepad = scorepads.get($stateParams.scorepadId);
 })
 .controller('gameDetailCtrl', function($scope, $stateParams, scorepads) {
+
+    // this function gets the relevant game object from the scorepad
     function gameDetails($stateParams, scorepads) {
         var games = scorepads.get($stateParams.scorepadId).games;
 
@@ -18,5 +20,26 @@ angular.module('scorepad.controllers', [])
         }
     }
 
+    // add the game to the scope
     $scope.game = gameDetails($stateParams, scorepads);
+
+    // function to handle interaction when a player taps on a player item
+    $scope.playerTap = function(event, elem) {
+        // hide any currently open button bars
+        console.log("You tapped an item. Hooray!");
+        console.log(elem);
+    }
+
+})
+
+.directive('detectGestures', function($ionicGesture) {
+    return {
+        link: function($scope,elem,attr) {
+        switch(attr.gestureType) {
+            case 'tap':
+                $ionicGesture.on('tap', $scope.playerTap, elem);
+                break;
+        } // switch
+        } // link
+    } // return
 });
